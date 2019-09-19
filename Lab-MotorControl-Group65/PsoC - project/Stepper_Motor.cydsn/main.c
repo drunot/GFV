a7c0963423ee3916e68f4b12bfb7d591bf7260df
drunot <antonvigen@hotmail.com>
2019-09-19 13:38:21 +0200

    Stepper motor updated

--- Lab-MotorControl-Group65/PsoC - project/Stepper_Motor.cydsn/main.c
+++ Lab-MotorControl-Group65/PsoC - project/Stepper_Motor.cydsn/main.c
@@ -66,16 +66,16 @@ CY_ISR(ISR_UART_rx_handler)
 //Initialize the three step modes
 uint8_t mode[][ARRAYLENGTH] = {
     //Pin output sequence for the waved-drive mode (repeated 2 times)
-    {0b0001, 0b0010, 0b0100, 0b1000,
-     0b0001, 0b0010, 0b0100, 0b1000},
+    {0b00000001, 0b00000010, 0b00000100, 0b00001000,
+     0b00000001, 0b00000010, 0b00000100, 0b00001000},
     
     //Pin output sequence for the full-step mode (repeated 2 times)
-    {0b0011, 0b0110, 0b1100, 0b1001,
-     0b0011, 0b0110, 0b1100, 0b1001},
+    {0b00000011, 0b00000110, 0b00001100, 0b00001001,
+     0b00000011, 0b00000110, 0b00001100, 0b00001001},
 
     //Pin output sequence for the half-step mode
-    {0b0001, 0b0011, 0b0010, 0b0110,
-     0b0100, 0b1100, 0b1000, 0b1001}
+    {0b00000001, 0b00000011, 0b00000010, 0b00000110,
+     0b00000100, 0b00001100, 0b00001000, 0b00001001}
 };
 
 //Mode selelction variable
@@ -87,10 +87,15 @@ uint8 modeSelect = 0;
 int8_t direction = FORWARD_;
 
 //Current step iterator
-uint8_t curr_step = 0;
+int8_t curr_step = 0;
 
 CY_ISR(ISR_step){
-    a1_Write(mode[modeSelect][curr_step+direction]);
+    curr_step += direction;
+    if(curr_step >= ARRAYLENGTH)
+        curr_step = 0;
+    else if(curr_step <= -1)
+        curr_step = ARRAYLENGTH -1;
+    a1_Write(mode[modeSelect][curr_step]);
 }
 
 void handleByteReceived(uint8_t byteReceived)
@@ -141,11 +146,15 @@ void decreaseSpeed()
     if(period > 400)
     {
         UART_1_PutString("Decreasing speed\r\n");
+        Timer_1_Stop();
         Timer_1_WritePeriod(period-200);
+        Timer_1_Start();
     }
     else if (period < 400){     
         UART_1_PutString("Min speed achieved\r\n");
+        Timer_1_Stop();
         Timer_1_WritePeriod(200);
+        Timer_1_Start();
     }
 }
 
@@ -155,12 +164,16 @@ void increaseSpeed()
     if(period < (65535-200))
     {
         UART_1_PutString("Increasing speed\r\n");
+        Timer_1_Stop();
         Timer_1_WritePeriod(period+200);
+        Timer_1_Start();
     }
     else
     {
         UART_1_PutString("Max speed achieved\r\n");
+        Timer_1_Stop();
         Timer_1_WritePeriod(65535);
+        Timer_1_Start();
     }
 }
 
