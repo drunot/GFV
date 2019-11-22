ad74fc32c77384ee7e54ac1f33b83e900eddcc6b
drunot <antonvigen@hotmail.com>
2019-11-22 14:16:39 +0100

    Ny data

--- Lab-TemperatureRegulator-Group65/PSoC/TempRegulator.cydsn/main.c
+++ Lab-TemperatureRegulator-Group65/PSoC/TempRegulator.cydsn/main.c
@@ -30,8 +30,8 @@ int main(void)
     UART_1_Start();
     PWM_1_Start();
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    float Kp = 2.0f;
-    float Ki = 1.0f/5.0f;
+    float Kp = 1.0f;
+    float Ki = 1.0f/10.0f;
     float Kd = 0;
     float integralMax = 3000;
     float integralMin = -3000;
