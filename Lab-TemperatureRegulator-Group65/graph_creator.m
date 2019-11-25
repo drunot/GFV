b2222dd847ee081c857b34e2413abbe7a8b66211
Mikkel Kirkegaard <fisk9889@gmail.com>
2019-11-25 17:09:54 +0100

    New figs

--- Lab-TemperatureRegulator-Group65/graph_creator.m
+++ Lab-TemperatureRegulator-Group65/graph_creator.m
@@ -3,7 +3,7 @@ clc
 close all
 clear
 
-data = readtable('capture6.txt');
+data = readtable('capture10.txt');
 
 newdata = table2array(data);
 
@@ -14,14 +14,14 @@ t = [0:1/3:(time-2)/3];
 figure(1); clf;
 plot(t,newdata(2:end,1:3));
 legend('TargetTemp','CurrTemp','Difference');
-title("Temperatur m?lingerne. Kp = 2, Ki = 30/30, Kd = 0");
+title("Temperatur m?lingerne. Kp = 20, Ki = 5/30, Kd = 1");
 xlabel("Tid [s]");
 ylabel("Amplitude [deg]");
 
 figure(2); clf;
 plot(t,newdata(2:end,4:end));
 legend('PWM','Partial','Integral','Differential');
-title("PWM m?lingerne. Kp = 2, Ki = 30/30, Kd = 0");
+title("PWM m?lingerne. Kp = 20, Ki = 5/30, Kd = 1");
 xlabel("Tid [s]");
 ylabel("Amplitude [%duty]");
 %%
