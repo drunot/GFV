9c553c1edc77b74bb1f9bb142c7f32e9a38a2cf9
viktorbjerring <54939110+viktorbjerring@users.noreply.github.com>
2019-11-25 18:46:11 +0100

    begår sudoku brb

--- Lab-TemperatureRegulator-Group65/graph_creator.m
+++ Lab-TemperatureRegulator-Group65/graph_creator.m
@@ -3,7 +3,7 @@ clc
 close all
 clear
 
-data = readtable('capture10.txt');
+data = readtable('capture17.txt');
 
 newdata = table2array(data);
 
@@ -14,14 +14,14 @@ t = [0:1/3:(time-2)/3];
 figure(1); clf;
 plot(t,newdata(2:end,1:3));
 legend('TargetTemp','CurrTemp','Difference');
-title("Temperatur m?lingerne. Kp = 20, Ki = 5/30, Kd = 1");
+title("Temperatur måling. Kp = 10, Ki = 3/30, Kd = 1");
 xlabel("Tid [s]");
 ylabel("Amplitude [deg]");
 
 figure(2); clf;
 plot(t,newdata(2:end,4:end));
 legend('PWM','Partial','Integral','Differential');
-title("PWM m?lingerne. Kp = 20, Ki = 5/30, Kd = 1");
+title("PWM måling. Kp = 10, Ki = 3/30, Kd = 1");
 xlabel("Tid [s]");
 ylabel("Amplitude [%duty]");
 %%
