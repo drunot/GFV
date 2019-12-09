1fea908cb9225ae0e3dea1da2365120de8d411d5
viktorbjerring <54939110+viktorbjerring@users.noreply.github.com>
2019-12-09 16:01:43 +0100

    tror stadig anton har mens

--- Secret DSB mappe/Ex4/encode_wav.m
+++ Secret DSB mappe/Ex4/encode_wav.m
@@ -1,43 +1,37 @@
 %% Loading af lyd
-clear
 
-[input, fs] = audioread('korobeiniki.wav');
+function[Compression, Size16bit, SizeEncoded] = encode_wav(filename)
+
+info = audioinfo(filename);
+[input, fs] = audioread(filename);
 T = 1/fs;
 L = length(input);
 t = (0:L-1)*T;
-
+if (fs < 40001)
+    disp('Sample Rate too low');
+    return
+end
+bitDepth = info.BitsPerSample;
+channels = info.NumChannels;
 
 %% DFT af signal, kvantisering til 16bit
 
-clear sound
 DFT = fft(input);
 f = fs*(0:(L/2))/L;
-temp = ifft(DFT);
-%sound(real(temp), fs);
-
-
-%%
-
-clear sound
 stopPoint = find(f>20000, 1);
 DFT = cat(1, DFT(1:stopPoint,:), zeros(L-stopPoint, 2));
 f = cat(2, f(1:stopPoint), zeros(1, L-stopPoint));
-P2 = abs(DFT/L);
-P1 = P2(1:L,:);
-P1(2:end-1) = 2*P1(2:end-1);
-semilogx(f, P1);
-temp = ifft(DFT);
-%sound(real(temp), fs);
 
 %% Kvantisering til 16 bits
 
-clear sound
 DFT16bit = quantise_n_bits(DFT, 16);
-temp = ifft(DFT16bit);
-sound(real(temp), fs);
+IFT = ifft(DFT16bit);
+encoded_file = sprintf("16bit_%s", filename); 
+audiowrite(encoded_file, real(IFT), fs);
+bitDepth = 16;
+Size16bit = bitDepth * L * channels;
 
-
-%% Adskilning af signal
+% Adskilning af signal
 
 point100Hz = find(f>100,1);
 point500Hz = find(f>500,1);
@@ -53,10 +47,10 @@ p501_1500Hz = DFT16bit(point500Hz+1:point1500Hz,:);
 p1501_3000Hz = DFT16bit(point1500Hz+1:point3000Hz,:);
 p3001_6000Hz = DFT16bit(point3000Hz+1:point6000Hz,:);
 p6001_10000Hz = DFT16bit(point6000Hz+1:point10000Hz,:);
-p10001_20000Hz = DFT16bit(point10000Hz+1:L,:);
+p10001_20000Hz = DFT16bit(point10000Hz+1:stopPoint,:);
 
 
-%% Kvantisering af dele
+% Kvantisering af dele
 %til 4 bit, 0-100 og 10001-20000 Hz
 kp0_100Hz = quantise_n_bits(p0_100Hz, 4);
 kp10001_20000Hz = quantise_n_bits(p10001_20000Hz, 4);
@@ -69,8 +63,17 @@ kp3001_6000Hz = quantise_n_bits(p3001_6000Hz, 12);
 %ubehandlet 1501-3000 Hz
 kp1501_3000Hz = p1501_3000Hz;
 
-%% sammensætning
-encoded = cat(1, kp0_100Hz, kp101_500Hz, kp501_1500Hz, kp1501_3000Hz, kp3001_6000Hz, kp6001_10000Hz, kp10001_20000Hz);
+% sammensætning
+encoded = cat(1, kp0_100Hz, kp101_500Hz, kp501_1500Hz, kp1501_3000Hz, kp3001_6000Hz, kp6001_10000Hz, kp10001_20000Hz, zeros(L-stopPoint, 2));
 
-temp = ifft(encoded);
-sound(real(temp), fs);
+IFT = ifft(encoded);
+encoded_file = sprintf("encoded_%s", filename);
+audiowrite(encoded_file, real(IFT), fs);
+
+% calculate size difference
+length4bit = length(kp0_100Hz) + length(kp10001_20000Hz);
+length6bit = length(kp101_500Hz) + length(kp6001_10000Hz);
+length12bit = length(kp501_1500Hz) + length(kp3001_6000Hz);
+length16bit = length(kp1501_3000Hz);
+SizeEncoded = channels * (4 * length4bit + 6 * length6bit + 12 * length12bit + 16 * length16bit);
+Compression = ((Size16bit-SizeEncoded) / Size16bit) * 100; 
